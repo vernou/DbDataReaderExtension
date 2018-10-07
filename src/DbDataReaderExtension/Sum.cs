fa43be85796196f350eb96f39c3b698dbd570fef
Vernou Cédric <Orwel@users.noreply.github.com>
2018-10-07 17:10:35 +0200

    #1 Initialize solution and projects (PR #2)

---
+++ src/DbDataReaderExtension/Sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DbDataReaderExtension
+{
+    public class Sum
+    {
+        private readonly int _a;
+        private readonly int _b;
+
+        public Sum(int a, int b)
+        {
+            _a = a;
+            _b = b;
+        }
+
+        public int Result => _a + _b;
+    }
+}
