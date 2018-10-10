b674291d35b9a2ab439a0d2a8d1127fd2827c48c
Vernou Cédric <Orwel@users.noreply.github.com>
2018-10-11 01:13:50 +0200

    #9 DbDataReaderExtension.GetFieldValue (PR #10)

--- src/DbDataReaderExtension.Test/DbDataReaderExtensionTest.cs
+++ src/DbDataReaderExtension.Test/DbDataReaderExtensionTest.cs
@@ -53,5 +53,51 @@ namespace DbDataReaderExtension.Test
                 }
             }
         }
+        
+        [Fact]
+        public void GetFieldValue()
+        {
+            using(var connection = new SqliteConnection("DataSource=:memory:"))
+            {
+                connection.Open();
+                using(var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"CREATE TABLE Foo (id NUMBER, label TEXT, price REAL);";
+                    command.ExecuteNonQuery();
+                    command.CommandText = @"INSERT INTO Foo values (1, 'Not Null', 2.5)";
+                    command.ExecuteNonQuery();
+                    command.CommandText = @"SELECT id, label, price from Foo";
+                    using(var reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        Assert.Equal(1, reader.GetFieldValue<int>("id"));
+                        Assert.Equal(2.5f, reader.GetFieldValue<float>("price"));
+                        Assert.Equal("Not Null", reader.GetFieldValue<string>("label"));
+                     }
+                }
+            }
+        }
+
+        [Fact]
+        public void GetFieldValueWithBadColumn()
+        {
+            using(var connection = new SqliteConnection("DataSource=:memory:"))
+            {
+                connection.Open();
+                using(var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"CREATE TABLE Foo (id NUMBER, label TEXT);";
+                    command.ExecuteNonQuery();
+                    command.CommandText = @"INSERT INTO Foo values (1, 'Not Null')";
+                    command.ExecuteNonQuery();
+                    command.CommandText = @"SELECT id, label from Foo";
+                    using(var reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        Assert.Throws<InvalidOperationException>(() => reader.GetFieldValue<int>("bad"));
+                    }
+                }
+            }
+        }
     }
 }
