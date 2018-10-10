d55044e94fd3b3972dcbca0ddcc95d28cf3c5de5
Vernou Cédric <Orwel@users.noreply.github.com>
2018-10-10 21:33:49 +0200

    #7 DbDataReaderExtension.IsDBNull (PR #8)

---
+++ src/DbDataReaderExtension/DbDataReaderExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.Common;
+
+namespace DbDataReaderExtension
+{
+    /// <summary>
+    /// Extension methods to DbDataReader
+    /// </summary>
+    public static class DbDataReaderExtension
+    {
+        /// <summary>
+        ///  Gets a value that indicates whether the column contains nonexistent or missing values.
+        /// </summary>
+        /// <param name="reader">The DbDataReader</param>
+        /// <param name="name">Column name</param>
+        /// <returns>true if the specified column is equivalent to DBNull; otherwise false.</returns>
+        /// <exception cref="System.InvalidOperationException">The column name is not valid.</param>
+        public static bool IsDBNull(this DbDataReader reader, string name)
+        {
+            return reader.IsDBNull(reader.Ordinal(name));
+        }
+
+        private static int Ordinal(this DbDataReader reader, string name)
+        {
+            try
+            {
+                return reader.GetOrdinal(name);
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                throw new InvalidOperationException($"The name '{name}' is not a valid column name.", ex);
+            }
+        }
+    }
+}
